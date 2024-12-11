ec5e47a5334e4a656e39444b5133fbc2bad38de9
Peterwicks <peterkasimba28.com>
2024-12-11 16:50:39 +0300

    my duka

--- main.py
+++ main.py
@@ -60,9 +60,55 @@ def thesales():
     else:
         cur.execute("select * from theproducts")
         theproducts= cur.fetchall()
-        cur.execute("select thesales.id, theproducts.name, thesales.quantity, thesales.created_at FROM thesales join theproducts on theproducts.id=thesales.pid")
+        cur.execute("select thesales.id, theproducts.name, thesales.quantity, thesales.created_at"\
+                     "FROM thesales join theproducts on theproducts.id=thesales.pid")
         thesales=cur.fetchall()
         
         return render_template("thesales.html", theproducts=theproducts, thesales=thesales)
+@app.route("/dashboard")
+def dashboard():
+    cur.execute("select sum(p.selling_price * s.quantity) as sales,"\
+                "s.created_at from thesales as s join theproducts as p on p.id =s.pid group by s.created_at;")
+    daily_sales = cur.fetchall()
+    #print(daily_sales)
+    x = [i[1].strftime('%d %m %Y') for i in daily_sales if float(i[0]) > 90000000]
+    y = [float(i[0]) for i in daily_sales if float(i[0]) > 90000000]
+    for i in daily_sales:
+       
+        return render_template("dashboard.html", x=x, y=y)
+
+#task 2: sales per product
+#x axis product name
+#y axis sale
+#bar graph group by product name
+@app.route("/dashboard_2")
+def dashboard_2(): 
+    cur.execute("select name as product_name, SUM(selling_price) AS total_sales from theproducts group by name ")
+    sales_per_product =cur.fetchall()
+    
+    
+    x=[s[0] for s in sales_per_product]
+    y=[float(s[1]) for s in sales_per_product]
+    for s in sales_per_product:
+        return render_template("dashboard_2.html", x=x, y=y)
+    
+@app.route("/login")
+def login():
+
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method=="GET":
+        return render_template("register.html")
+    else:
+        name= request.form["Name"]
+        email=request.form["Email"]
+        password= request.form["Password"]
+        query= "insert into users (name, email, password) values ('{}', '{}', '{}')".format(name,email,password)
+        cur.execute(query)
+        conn.commit()
+        return redirect ("/dashboard")
+
 
 app.run(debug=True)
